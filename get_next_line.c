dee7e201150634c08222e9c63cb3085be76d2625
lsadikaj <leotrimsad@gmail.com>
2024-11-19 14:39:53 +0100

    aSGardJ19nOV

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lsadikaj <lsadikaj@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 09:38:20 by lsadikaj          #+#    #+#             */
-/*   Updated: 2024/11/14 11:02:05 by lsadikaj         ###   ########.fr       */
+/*   Updated: 2024/11/19 14:39:18 by lsadikaj         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,27 +60,23 @@ static char	*fill_line_buffer(int fd, char *left_c)
 	return (left_c);
 }
 
-static char	*set_line(char **left_c)
+static char	*extract_line(char **left_c)
 {
+	size_t	i;
 	char	*line;
 	char	*new_left_c;
-	size_t	i;
 
-	i = 0;
 	if (!*left_c || **left_c == '\0')
 		return (NULL);
-	while ((*left_c)[i] != '\n' && (*left_c)[i] != '\0')
+	i = 0;
+	while ((*left_c)[i] && (*left_c)[i] != '\n')
 		i++;
 	if ((*left_c)[i] == '\n')
-	{
-		line = ft_substr(*left_c, 0, i + 1);
-		new_left_c = ft_strdup(*left_c + i + 1);
-	}
-	else
-	{
-		line = ft_strdup(*left_c);
-		new_left_c = NULL;
-	}
+		i++;
+	line = ft_substr(*left_c, 0, i);
+	if (!line)
+		return (NULL);
+	new_left_c = ft_strdup(*left_c + i);
 	free(*left_c);
 	*left_c = new_left_c;
 	return (line);
@@ -101,7 +97,7 @@ char	*get_next_line(int fd)
 	left_c = fill_line_buffer(fd, left_c);
 	if (!left_c)
 		return (NULL);
-	line = set_line(&left_c);
+	line = extract_line(&left_c);
 	if (!line)
 	{
 		free(left_c);
